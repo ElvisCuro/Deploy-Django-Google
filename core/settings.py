ab895405548776e84ad07fb0109d6be4c32562de
ElvisCuro <elviscuro0@gmail.com>
2023-11-30 14:23:16 -0500

    update static

--- core/settings.py
+++ core/settings.py
@@ -98,7 +98,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR.joinpath('dist')],
+        'DIRS': [BASE_DIR.joinpath('static')],
         'APP_DIRS': True,
         'OPTIONS': {
            'context_processors': [
@@ -195,11 +195,11 @@ STATIC_URL = '/static/'
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'dist', 'assets'),  # Ruta a la carpeta 'assets' donde están los archivos estáticos
